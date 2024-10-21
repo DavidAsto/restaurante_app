0bd3f3db5acaaf5b93fbd0dbd13378d18f94450d
DavidAsto <davidasto2001@gmail.com>
2024-10-21 00:55:11 -0500

    ultima subida

--- pages/includes/cabecera.php
+++ pages/includes/cabecera.php
@@ -2,7 +2,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<<<<<<< HEAD
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?=$ruta?>/css/bootstrap/css/bootstrap.min.css" />
     
@@ -28,7 +27,6 @@
     <!-- Script personalizado -->
     <script src="<?=$ruta?>/js/codigo.js"></script>
 </body>
-=======
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!--<link rel="stylesheet" type="text/css" href="<?=$ruta?>/css/bootstrap/css/bootstrap.css" />-->
     <link rel="stylesheet" type="text/css" href="<?=$ruta?>/css/fontawesome/css/all.css" />
@@ -41,4 +39,4 @@
 
     <title><?=$titulo?></title>
 </head>
->>>>>>> 5ae995f82bb435f662c96f6f8eeaca5753473ab3
+
--- pages/includes/menu.php
+++ pages/includes/menu.php
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-<nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #ffb347;">
-=======
+
+
 <nav class="navbar navbar-expand-lg" style="background-color: #ffb347;">
->>>>>>> 5ae995f82bb435f662c96f6f8eeaca5753473ab3
+
   <div class="container-fluid">
     <a class="navbar-brand" href="#"><strong><span class="fo">fo</span>odtech</strong></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,17 +10,15 @@
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-<<<<<<< HEAD
+
           <a class="nav-link" href="inicio.php">Inicio</a>
-=======
-          <a class="nav-link" href="#">Inicio</a>
->>>>>>> 5ae995f82bb435f662c96f6f8eeaca5753473ab3
+
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Nosotros</a>
         </li>
         <li class="nav-item">
-<<<<<<< HEAD
+
           <a class="nav-link admin" href="listar_cliente.php">Cliente</a>
         </li>
         <li class="nav-item">
@@ -33,25 +30,10 @@
         <li class="nav-item">
           <a class="nav-link admin" href="listar_categoria.php">Categoria</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link admin" href="contacto.php">Contacto</a>
-=======
-          <a class="nav-link admin" href="#">Cliente</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link admin" href="#">Comida</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link admin" href="#">Bebida</a>
->>>>>>> 5ae995f82bb435f662c96f6f8eeaca5753473ab3
-        </li>
       </ul>
     </div>
   </div>
-<<<<<<< HEAD
-</nav>
-=======
+
 </nav>
 
-<!--bg-body-tertiary-->
->>>>>>> 5ae995f82bb435f662c96f6f8eeaca5753473ab3
+
